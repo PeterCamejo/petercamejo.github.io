fe6cd99a6c80a3d84582f8d4961a67572bd4447a
Peter Camejo <thealex123@gmail.com>
2022-03-26 20:43:33 -0400

    fixed fade in js for portfolio page

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 
 
 $(document).ready(function(){
-	$('.fade-first').each(function(){
-		$(this).animate({'opacity':'1'}, 800);
+	$('.card').each(function(){
+			const card_bottom = $(this).position().top +
+								$(this).outerHeight();
+			const win_bottom = $(window).scrollTop() +
+									$(window).height();
+			if(win_bottom > card_bottom){
+				$(this).animate({'opacity':'1'}, 500);
+			}
 	});
 	$(window).scroll(function(){
 		$('.card').each(function(){
-			var card_bottom = $(this).position().top +
+			const card_bottom = $(this).position().top +
 								$(this).outerHeight();
-			var win_bottom = $(window).scrollTop() +
+			const win_bottom = $(window).scrollTop() +
 									$(window).height();
 			if(win_bottom > card_bottom){
 				$(this).animate({'opacity':'1'}, 500);
